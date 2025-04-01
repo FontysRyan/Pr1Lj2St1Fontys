f28bd7c631a23822c3f9023f477c5f39ab3f81aa
Ryan <547558@student.fontys.nl>
2025-04-01 10:33:28 +0200

    Updated database talking for overzichtpage
    Now it will only get the data it needs. Since we open connection here, talk to database we only want to receive the data we want instead of all data, order it to startdate now, store them in a list of the Eventmodel object

--- Pr1Lj2St1Fontys_webapp/Pages/Overzicht.cshtml.cs
+++ Pr1Lj2St1Fontys_webapp/Pages/Overzicht.cshtml.cs
@@ -7,20 +7,28 @@ namespace Pr1Lj2St1Fontys_webapp.Pages
 {
     public class OverzichtModel : PageModel
     {
-        private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context; // Database context to access the database
 
-        public IEnumerable<EventModel> Evenementen { get; set; }
+        public IEnumerable<EventModel> Evenementen { get; set; } // List of EventModel objects to store the events retrieved from the database
 
-        public OverzichtModel(ApplicationDbContext context)
+        public OverzichtModel(ApplicationDbContext context) // Constructor to get the database context injected into this class
         {
             _context = context;
         }
 
         public void OnGet()
         {
-            // Haal de evenementen uit de database
-            Evenementen = _context.Events.AsNoTracking().ToList(); // Gebruik EventModel voor Evenementen
+            // Retrieve only future events from the database
+            Evenementen = _context.Events // Access the Events table in the database
+                .AsNoTracking() // No tracking of objects, because we only read from the database and don't change anything also optimizes performance
+                .Where(e => e.StartDatumTijd > DateTime.Now) // Only upcoming events
+                .OrderBy(e => e.StartDatumTijd) // Sort ascending
+                .ToList(); // Executes query and stores results in a list of EventModel objects 
         }
+        //SELECT* FROM Evenementen
+        //WHERE StartDatumTijd > GETDATE()
+        //ORDER BY StartDatumTijd;
+
     }
 }
 
